97469d2bc29af756ebf1388f6f45df6bcf5bcc1c
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-02-17 22:19:56 +0300

    Create students.dart

---
+++ lib/screens/students.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StudentsScreen extends StatelessWidget {
+  const StudentsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Students")),
+      body: const Center(child: Text("Student management coming here")),
+    );
+  }
+}
